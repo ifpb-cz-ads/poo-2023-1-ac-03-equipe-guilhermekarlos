fe1b701ac9b8f3c514b507ccfd8d42cd5b001dad
Guilherme Henrique <guilherme.pessoa@academico.ifpb.edu.br>
2023-08-29 15:05:21 -0300

    Adicionando arquivos das resoluções das questões práticas

---
+++ BMExercicio02.java
@@ -0,0 +1,25 @@
+/*Escreva um programa que declare, inicialize e imprima as seguintes variáveis:
+• Inteiro i de 32 bits com valor 1
+• Inteiro j de 64 bits com valor 2
+• Ponto flutuante p de 32 bits com valor 20.0
+• Ponto flutuante q de 64 bits com valor 30.0
+• Boolean b com valor verdadeiro
+• Caracter c com valor ‘k’ */
+
+public class BMExercicio02 {
+    public static void main(String[] args){
+        int i = 1;
+        long j = 2;
+        float p = 20.0f;
+        double q = 30.0;
+        boolean b = true;
+        char c = 'k';
+
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        System.out.println("p = " + p);
+        System.out.println("q = " + q);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+    }
+}
---
+++ CBBAtividadePratica2.java
@@ -0,0 +1,11 @@
+public class CBBAtividadePratica2 {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int soma = a + b + c;
+        int media = soma / 3;
+        System.out.println("A soma dos valores é: " + soma);
+        System.out.println("A média dos valores é: " + media);
+    }
+}
