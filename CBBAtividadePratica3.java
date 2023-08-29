424714195769b497cd45743369cd204b69b368d1
Karlos Messyas <karlosmessyas14@gmail.com>
2023-08-29 15:09:49 -0300

    Adicionando questões BM03 E CBB03

---
+++ CBBAtividadePratica3.java
@@ -0,0 +1,15 @@
+/*Crie um programa que calcule a área de um retângulo, declare e 
+inicialize duas variáveis para testá-lo, imprimindo esses resultados 
+na tela.*/
+
+public class CBBAtividadePratica3{
+    public static void main(String[] args){
+        int base, altura;
+        base = 10;
+        altura = 5;
+        int area;
+        area = base*altura;
+        System.out.println("A área do retângulo com base " + base + " e altura " + altura + " é de " + area);
+    }
+    
+}
